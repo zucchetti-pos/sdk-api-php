830cef438016dd21994b750d31add153d36c311f
Jhon Mike <developer@jhonmike.com.br>
2017-03-31 14:20:59 -0300

    first commit

---
+++ src/Service/Compufacil.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Compufacil\Service;
+
+class Compufacil
+{
+    private $token;
+    private $config;
+    private $defaultConfig = [
+        'url' => 'http://homolog.compufacil.com.br',
+        'version' => '1'
+    ];
+
+    public function __construct(array $config = [])
+    {
+        $this->config = $config;
+    }
+
+    public function signIn(string $email, string $password)
+    {
+        $config = $this->getConfig();
+        $params = [
+            'url' => $config['url'].'/rpc/v'.$config['version'].'/application.authenticate.json',
+            'content' => [
+                'login' => $email,
+                'password' => $password
+            ]
+        ];
+
+        $result = $this->streamService($params);
+
+        $this->setToken($result['access_token']);
+        return $result;
+    }
+
+    public function rpcService(string $serviceName, array $params) : array
+    {
+        if (empty($this->token)) {
+            throw new \Exception("Invalid token", 1);
+        }
+
+        $config = $this->getConfig();
+        $data = [
+            'url' => $config['url'].'/rpc/v'.$config['version'].'/'.$serviceName,
+            'content' => $params
+        ];
+
+        return $this->streamService($data);
+    }
+
+    public function streamService(array $data) : array
+    {
+        $opts = [
+            'http' => [
+                'method' => 'POST',
+                'header' => "Content-type: application/x-www-form-urlencoded",
+                'content' => http_build_query($data['content']),
+            ],
+        ];
+
+        if ($this->token) {
+            $opts['http']['header'] .= "\nAuthorization-Compufacil: ".$this->token;
+        }
+
+        $context = stream_context_create($opts);
+        $result = file_get_contents($data['url'], false, $context);
+
+        return json_decode($result, true);
+    }
+
+    public function setConfig($config) : Compufacil
+    {
+        $this->config = $config;
+        return $this;
+    }
+
+    public function getConfig() : array
+    {
+        return array_merge_recursive($this->defaultConfig, $this->config);
+    }
+
+    public function setToken($token) : Compufacil
+    {
+        $this->token = $token;
+        return $this;
+    }
+
+    public function getToken()
+    {
+        return $this->token;
+    }
+}
---
+++ tests/Service/CompufacilTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace CompufacilTest\Service;
+
+use Compufacil\Service\Compufacil;
+
+class CompufacilTest extends \PHPUnit\Framework\TestCase
+{
+    public function testSignUp_returnId_whenPassedData()
+    {
+        $service = new Compufacil();
+        $result = $service->signIn('jean@compufacil.com.br', '123456');
+        static::assertTrue(isset($result['access_token']));
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage Invalid token
+     */
+    public function testRpcService_returnExceptionToken_whenGetClientWithoutToken()
+    {
+        $service = new Compufacil();
+        $result = $service->rpcService('person.get-person.json', [
+            'isClient' => true,
+            'maxResults' => 10,
+            'page' => 1
+        ]);
+    }
+
+    public function testRpcService_returnDataListClients_whenGetClientWithoutToken()
+    {
+        $service = new Compufacil();
+        $dataLogin = $service->signIn('jean@compufacil.com.br', '123456');
+        $service->setToken($dataLogin['access_token']);
+
+        $result = $service->rpcService('person.get-person.json', [
+            'isClient' => true,
+            'maxResults' => 10,
+            'page' => 1
+        ]);
+
+        static::assertTrue(isset($result['data']));
+    }
+}
